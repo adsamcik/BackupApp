ea92bf098119fdb402eea7b074a20d26cda1c80a
adsamcik <adsamcik@users.noreply.github.com>
2016-06-14 12:01:30 +0200

    Improved config doc

--- BackupApp/Config.h
+++ BackupApp/Config.h
@@ -13,31 +13,55 @@ public:
 	~Config();
 
 	/**
-		\return Success (Successfull when valid path and not already added)
+		@return Success (Successfull when valid path and not already added)
 	*/
 	const ext::Success AddPath(const std::string& path);
 
 	/**
-		\return Success (Successfull when path is found)
+		@return Success (Successfull when path is found)
 	*/
 	const ext::Success RemovePath(const std::string& path);
 	const ext::Success RemovePath(const size_t& path);
 
 	/**
-		\return Success
+		@return Success
 	*/
 	const ext::Success Save();
 
 	void Edit(FileManager &fm);
 
 	/**
-		User input functions
-		Start with U
+		User input function to set a day
+		@param param parameters (without function call) 
 	*/
-	void USetDay(const std::string& line);
+	void USetDay(const std::string& param);
+
+	/**
+		User input function
+		Draws a list to cout of backed up paths
+		@param param parameters (without function call)
+	*/
 	void UList();
-	void UAdd(FileManager &fm, const std::string& line);
-	void URemove(FileManager &fm, const std::string& line);
+
+	/**
+		User input function
+		Checks if path is valid and if so adds it to backed up list
+		@param param parameters (without function call)
+	*/
+	void UAdd(FileManager &fm, const std::string& param);
+
+	/**
+		User input function
+		Remove path by string
+		@param param parameters (without function call)
+	*/
+	void URemove(FileManager &fm, const std::string& param);
+
+	/**
+		User input function
+		Remove path by index
+		@param index index (can be found with list function)
+	*/
 	void URemove(FileManager &fm, const size_t& index);
 
 	///paths to back up
@@ -50,7 +74,9 @@ private:
 	///Day on which files should be backed up
 	ext::DayOfWeek day;
 
-
+	/**
+		Prints help for Config to cout
+	*/
 	void PrintOptions();
 };
 
