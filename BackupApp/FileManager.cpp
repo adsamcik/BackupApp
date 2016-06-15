2fd424184854d666a9c88f11ada90a13b627065c
adsamcik <adsamcik@users.noreply.github.com>
2016-06-15 16:52:45 +0200

    Rewritten data offsetting

--- BackupApp/FileManager.cpp
+++ BackupApp/FileManager.cpp
@@ -29,7 +29,7 @@ FileManager::FileManager() {
 		File *f;
 		do {
 			f = new File(*stream);
-			files.push_back(f);	
+			files.push_back(f);
 		} while (stream->seekg(f->endContent).peek() != EOF);
 	}*/
 	Close();
@@ -66,7 +66,7 @@ void FileManager::Backup(File *file, const std::streampos &beg) {
 
 	if (file->beginMeta == -1) {
 		file->beginMeta = beg;
-		file->beginContent = file->beginMeta + 12 + sizeof(file->endContent) + file->GetPath()->size();
+		file->beginContent = sizeof(uint32_t) + sizeof(time_t) + sizeof(file->endContent) + file->GetPath()->size();
 		file->endContent = file->beginContent + length;
 		stream->seekg(beg);
 		file->WriteMeta(stream);
@@ -107,13 +107,35 @@ void FileManager::Backup(Dir *dir) {
 	delete v;
 }
 
+/**
+	Offset all data till the end of file
+	Will cause terrible problems if beg is not at the beginning of a file!
+*/
 void FileManager::OffsetData(const std::streampos &beg, const std::streamoff &off) {
-	long long length = fileEnd;
-	for (auto file : files) {
-		if (file->beginContent > beg) {
-			file->beginContent += off;
-			file->endContent += off;
-		}
+	//is signed to save on conversion when using in seek
+	const int32_t bufferSize = 512;
+	char *buffer = new char[bufferSize];
+	stream->seekg(fileEnd);
+	auto pos = fileEnd;
+	stream->seekg(-bufferSize, std::ios::end);
+	do {
+		stream->seekg(-bufferSize, std::ios::cur);
+		pos = stream->tellg();
+		stream->read(buffer, bufferSize);
+		stream->seekg(off - bufferSize, std::ios::cur);
+		stream->write(buffer, bufferSize);
+		stream->seekg(pos);
+	} while (stream->tellg() - beg > bufferSize);
+	delete[] buffer;
+
+	auto diff = stream->tellg() - beg;
+	if (diff > 0) {
+		stream->seekg(beg);
+		buffer = new char[diff];
+		stream->read(buffer, diff);
+		stream->seekg(off - diff, std::ios::cur);
+		stream->write(buffer, diff);
+		delete[] buffer;
 	}
 }
 
@@ -121,9 +143,9 @@ void FileManager::OffsetData(const std::streampos &beg, const std::streamoff &of
 void FileManager::RebuildBackups() {}
 
 void FileManager::BackupAll() {
+	auto paths = Config::paths;
 	Open();
-	
-	for (auto file : files) {
+	/*for (auto file : paths) {
 		if (!file->IsValid())
 			Console::PrintError(*file->GetPath() + " is not a valid path!");
 		else {
@@ -135,6 +157,6 @@ void FileManager::BackupAll() {
 				Backup(file, fileEnd);
 		}
 		//file->ClearPath();
-	}
+	}*/
 	Close();
 }
